11dd2f77557a010a3a3c80d69908dd109d4b06d7
Daniel Hu <daniel_hu4@brown.edu>
2025-06-21 20:07:25 -0400

    changed vite files

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/lit-distro/',  // Set the base path for GitHub Pages or your subdirectory
+  base: '/ui-starter/',  // Set the base path for GitHub Pages or your subdirectory
   plugins: [react()]
 })
